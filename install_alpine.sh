3c7ee4e3cdde9c882ecbb5ee822f40c8d1cdc1a5
Suyunmeng <sumengjing@outlook.com>
2025-06-10 11:57:12 +0800

    Update install_alpine.sh

--- install_alpine.sh
+++ install_alpine.sh
@@ -1,171 +1,161 @@
-#!/bin/bash
+#!/bin/sh
 
 red='\033[0;31m'
 green='\033[0;32m'
+blue='\033[0;34m'
 yellow='\033[0;33m'
 plain='\033[0m'
 
 cur_dir=$(pwd)
 
-# check root
-[[ $EUID -ne 0 ]] && echo -e "${red}严重错误: ${plain} 请以 root 权限运行此脚本 \n " && exit 1
+[ $(id -u) -ne 0 ] && echo "\033[0;31mFatal error: \033[0m Please run this script with root privilege\n" && exit 1
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    release=$ID
+elif [ -f /usr/lib/os-release ]; then
+    . /usr/lib/os-release
+    release=$ID
+else
+    echo "Failed to check the system OS, please contact the author!" >&2
+    exit 1
+fi
+echo "The OS release is: $release"
+
+arch() {
+    case "$(uname -m)" in
+    x86_64 | x64 | amd64) echo 'amd64' ;;
+    i*86 | x86) echo '386' ;;
+    armv8* | armv8 | arm64 | aarch64) echo 'arm64' ;;
+    armv7* | armv7 | arm) echo 'armv7' ;;
+    armv6* | armv6) echo 'armv6' ;;
+    armv5* | armv5) echo 'armv5' ;;
+    s390x) echo 's390x' ;;
+    *) echo "${green}Unsupported CPU architecture! ${plain}" && rm -f install.sh && exit 1 ;;
+    esac
+}
+
+echo "Arch: $(arch)"
+
+check_musl_version() {
+    if command -v ldd >/dev/null 2>&1 && ldd --version 2>&1 | grep -qi musl; then
+        musl_version=$(ldd --version 2>&1 | head -n1 | grep -oE '[0-9]+\.[0-9]+(\.[0-9]+)?')
+        required_version="1.2.0"
+        if [ "$(printf '%s\n' "$required_version" "$musl_version" | sort -V | head -n1)" != "$required_version" ]; then
+            echo "${red}musl libc version $musl_version is too old! Required: 1.2.0 or higher${plain}"
+            echo "Please upgrade Alpine to a newer version with musl >= 1.2.0."
+            exit 1
+        fi
+        echo "musl libc version: $musl_version (meets requirement of 1.2.0+)"
+    else
+        echo "${red}musl libc not detected. This script requires musl-based system.${plain}"
+        exit 1
+    fi
+}
+check_musl_version
 
 install_base() {
-	apk add --no-cache --update ca-certificates tzdata fail2ban bash
-	rm -f /etc/fail2ban/jail.d/alpine-ssh.conf
-	cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
-	sed -i "s/^\[ssh\]$/&\nenabled = false/" /etc/fail2ban/jail.local
-	sed -i "s/^\[sshd\]$/&\nenabled = false/" /etc/fail2ban/jail.local
-	sed -i "s/#allowipv6 = auto/allowipv6 = auto/g" /etc/fail2ban/fail2ban.conf
-}
-
-gen_random_string() {
-    local length="$1"
-    local random_string=$(LC_ALL=C tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w "$length" | head -n 1)
-    echo "$random_string"
-}
-
-config_after_install() {
-    local existing_username=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo 'username: .+' | awk '{print $2}')
-    local existing_password=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo 'password: .+' | awk '{print $2}')
-    local existing_webBasePath=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo 'webBasePath: .+' | awk '{print $2}')
-    local existing_port=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo 'port: .+' | awk '{print $2}')
-    local server_ip=$(curl -s https://api.ipify.org)
-
-    if [[ ${#existing_webBasePath} -lt 4 ]]; then
-        if [[ "$existing_username" == "admin" && "$existing_password" == "admin" ]]; then
-            local config_webBasePath=$(gen_random_string 15)
-            local config_username=$(gen_random_string 10)
-            local config_password=$(gen_random_string 10)
-
-            read -p "您是否需要自定义面板端口? (如果不需要自定义, 系统将随机生成端口) [y/n]: " config_confirm
-            if [[ "${config_confirm}" == "y" || "${config_confirm}" == "Y" ]]; then
-                read -p "请输入面板自定义端口: " config_port
-                echo -e "${yellow}您的面板端口是: ${config_port}${plain}"
-            else
-                local config_port=$(shuf -i 1024-62000 -n 1)
-                echo -e "${yellow}生成的随机端口: ${config_port}${plain}"
-            fi
-
-            /usr/local/x-ui/x-ui setting -username "${config_username}" -password "${config_password}" -port "${config_port}" -webBasePath "${config_webBasePath}"
-            echo -e "这是全新安装, 出于安全考虑, 会生成随机登录信息:"
-            echo -e "###############################################"
-            echo -e "${green}用户名: ${config_username}${plain}"
-            echo -e "${green}密码: ${config_password}${plain}"
-            echo -e "${green}端口: ${config_port}${plain}"
-            echo -e "${green}面板路径: ${config_webBasePath}${plain}"
-            echo -e "${green}访问面板URL: http://${server_ip}:${config_port}/${config_webBasePath}${plain}"
-            echo -e "###############################################"
-            echo -e "${yellow}如果你忘记了登录信息, 你可以使用命令“x-ui settings”${plain}"
-        else
-            local config_webBasePath=$(gen_random_string 15)
-            echo -e "${yellow}面板路径缺失或太短, 正在生成一个新的...${plain}"
-            /usr/local/x-ui/x-ui setting -webBasePath "${config_webBasePath}"
-            echo -e "${green}新的面板路径: ${config_webBasePath}${plain}"
-            echo -e "${green}访问面板URL: http://${server_ip}:${existing_port}/${config_webBasePath}${plain}"
-        fi
-    else
-        if [[ "$existing_username" == "admin" && "$existing_password" == "admin" ]]; then
-            local config_username=$(gen_random_string 10)
-            local config_password=$(gen_random_string 10)
-
-            echo -e "${yellow}检测到默认用户名和密码, 需要安全更新...${plain}"
-            /usr/local/x-ui/x-ui setting -username "${config_username}" -password "${config_password}"
-            echo -e "生成新的随机登录用户名和密码:"
-            echo -e "###############################################"
-            echo -e "${green}用户名: ${config_username}${plain}"
-            echo -e "${green}密码: ${config_password}${plain}"
-            echo -e "###############################################"
-            echo -e "${yellow}如果你忘记了登录信息, 你可以使用命令“x-ui settings”${plain}"
-        else
-            echo -e "${green}用户名, 密码和面板路径已正确设置. 退出...${plain}"
-        fi
-    fi
-
-    /usr/local/x-ui/x-ui migrate
+    case "$release" in
+    alpine)
+        apk update && apk add --no-cache wget curl tar tzdata
+        ;;
+    ubuntu | debian | armbian)
+        apt-get update && apt-get install -y -q wget curl tar tzdata
+        ;;
+    centos | almalinux | rocky | ol)
+        yum -y update && yum install -y -q wget curl tar tzdata
+        ;;
+    fedora | amzn | virtuozzo)
+        dnf -y update && dnf install -y -q wget curl tar tzdata
+        ;;
+    arch | manjaro | parch)
+        pacman -Syu && pacman -Syu --noconfirm wget curl tar tzdata
+        ;;
+    opensuse-tumbleweed)
+        zypper refresh && zypper -q install -y wget curl tar timezone
+        ;;
+    *)
+        apt-get update && apt install -y -q wget curl tar tzdata
+        ;;
+    esac
 }
 
 install_x-ui() {
-	cd /usr/local
-    if [ $# == 0 ]; then
-        tag_version=$(curl -Ls "https://api.github.com/repos/56idc/3x-ui-alpine/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-        if [[ ! -n "$tag_version" ]]; then
-            echo -e "${red}获取x-ui版本失败, 可能是GitHub API限制或网络连接失败, 请检查您的网络连接后重试...${plain}"
+    cd /usr/local/
+
+    if [ $# -eq 0 ]; then
+        tag_version=$(curl -Ls "https://api.github.com/repos/Suyunmeng/3x-ui-alpine/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^\"]+)".*/\1/')
+        if [ -z "$tag_version" ]; then
+            echo "${red}Failed to fetch x-ui version, it may be due to GitHub API restrictions, please try it later${plain}"
             exit 1
         fi
-        echo -e "获取x-ui最新版本: ${tag_version}, 开始安装..."
-        wget --no-check-certificate -O /usr/local/x-ui-linux-alpine.tar.gz https://github.com/56idc/3x-ui-alpine/releases/download/${tag_version}/x-ui-linux-alpine.tar.gz
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui 失败, 请确保你的服务器可以访问 GitHub ${plain}"
+        echo "Got x-ui latest version: ${tag_version}, beginning the installation..."
+        wget -N -O /usr/local/x-ui-linux-$(arch).tar.gz https://github.com/Suyunmeng/3x-ui-alpine/releases/download/${tag_version}/x-ui-linux-$(arch).tar.gz || {
+            echo "${red}Downloading x-ui failed, please be sure that your server can access GitHub ${plain}"
             exit 1
-        fi
+        }
     else
         tag_version=$1
         tag_version_numeric=${tag_version#v}
-        min_version="2.4.8"
-        if [[ "$(printf '%s\n' "$min_version" "$tag_version_numeric" | sort -V | head -n1)" != "$min_version" ]]; then
-            echo -e "${red}请使用较新的版本(至少 v2.4.8), 退出安装...${plain}"
+        min_version="2.3.5"
+        if [ "$(printf '%s\n' "$min_version" "$tag_version_numeric" | sort -V | head -n1)" != "$min_version" ]; then
+            echo "${red}Please use a newer version (at least v2.3.5). Exiting installation.${plain}"
             exit 1
         fi
-
-        url="https://github.com/56idc/3x-ui-alpine/releases/download/${tag_version}/x-ui-linux-alpine.tar.gz"
-        echo -e "开始安装x-ui $1"
-        wget --no-check-certificate -O /usr/local/x-ui-linux-alpine.tar.gz ${url}
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载x-ui $1 失败, 请检查版本是否存在 ${plain}"
+        url="https://github.com/Suyunmeng/3x-ui-alpine/releases/download/${tag_version}/x-ui-linux-$(arch).tar.gz"
+        echo "Beginning to install x-ui $1"
+        wget -N -O /usr/local/x-ui-linux-$(arch).tar.gz ${url} || {
+            echo "${red}Download x-ui $1 failed, please check if the version exists ${plain}"
             exit 1
-        fi
+        }
     fi
 
-    if [[ -e /usr/local/x-ui/ ]]; then
-      echo -e "卸载旧版本..."
-      rc-update del x-ui
-      rc-service x-ui stop
-      fail2ban-client -x stop
-      rm /usr/local/x-ui/ -rf
-      rm /etc/init.d/x-ui
-	  if [[ -e /app/bin/ ]]; then
-	    pgrep -f x-ui | xargs -r kill -9
-            rm /app -rf
-	  fi
-    fi
+    [ -e /usr/local/x-ui/ ] && [ -x /etc/init.d/x-ui ] && /etc/init.d/x-ui stop && rm -rf /usr/local/x-ui/
 
-    tar zxvf x-ui-linux-alpine.tar.gz
-    rm x-ui-linux-alpine.tar.gz -f
-    mv x-ui/app/* x-ui
-    rm x-ui/app -rf
-    rm x-ui/DockerEntrypoint.sh
-    chmod +x x-ui/x-ui x-ui/bin/xray-linux-amd64
-    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/56idc/3x-ui-alpine/main/x-ui-alpine.sh
-    chmod +x /usr/bin/x-ui
-    wget --no-check-certificate -O /etc/init.d/x-ui https://raw.githubusercontent.com/56idc/3x-ui-alpine/main/x-ui.rc
+    tar zxvf x-ui-linux-$(arch).tar.gz
+    rm -f x-ui-linux-$(arch).tar.gz
+    cd x-ui
+    chmod +x x-ui
+
+    case $(arch) in
+        armv5|armv6|armv7)
+            mv bin/xray-linux-$(arch) bin/xray-linux-arm
+            chmod +x bin/xray-linux-arm
+            ;;
+    esac
+
+    chmod +x x-ui bin/xray-linux-$(arch)
+    cp -f x-ui.rc /etc/init.d/x-ui
     chmod +x /etc/init.d/x-ui
-    config_after_install
-    export XRAY_VMESS_AEAD_FORCED="false"
-    fail2ban-client -x start
     rc-update add x-ui default
-    rc-service x-ui start
-    echo -e "${green}x-ui ${tag_version}${plain} 安装完成, 运行中..."
-    echo -e ""
-    echo -e "x-ui子命令菜单:"
-    echo -e "----------------------------------------------"
-    echo -e "x-ui              - 主菜单"
-    echo -e "x-ui start        - 运行服务"
-    echo -e "x-ui stop         - 停止服务"
-    echo -e "x-ui restart      - 重启服务"
-    echo -e "x-ui status       - 查看服务状态"
-    echo -e "x-ui settings     - 查看服务配置"
-    echo -e "x-ui enable       - 打开服务开机自动启动"
-    echo -e "x-ui disable      - 关闭服务开机自动启动"
-    echo -e "x-ui log          - 查看日志"
-    echo -e "x-ui banlog       - 查看Fail2ban日志"
-    echo -e "x-ui update       - 升级"
-    echo -e "x-ui legacy       - 安装旧版本"
-    echo -e "x-ui install      - 安装"
-    echo -e "x-ui uninstall    - 卸载"
-    echo -e "----------------------------------------------"
+    /etc/init.d/x-ui start
+
+    wget -O /usr/bin/x-ui https://raw.githubusercontent.com/Suyunmeng/3x-ui-alpine/main/x-ui.sh
+    chmod +x /usr/local/x-ui/x-ui.sh
+    chmod +x /usr/bin/x-ui
+
+    echo "${green}x-ui ${tag_version}${plain} installation finished, it is running now..."
+    echo ""
+    echo "┌───────────────────────────────────────────────────────┐"
+    echo "│  ${blue}x-ui control menu usages (subcommands):${plain}              │"
+    echo "│                                                       │"
+    echo "│  ${blue}x-ui${plain}              - Admin Management Script          │"
+    echo "│  ${blue}x-ui start${plain}        - Start                            │"
+    echo "│  ${blue}x-ui stop${plain}         - Stop                             │"
+    echo "│  ${blue}x-ui restart${plain}      - Restart                          │"
+    echo "│  ${blue}x-ui status${plain}       - Current Status                   │"
+    echo "│  ${blue}x-ui settings${plain}     - Current Settings                 │"
+    echo "│  ${blue}x-ui enable${plain}       - Enable Autostart on OS Startup   │"
+    echo "│  ${blue}x-ui disable${plain}      - Disable Autostart on OS Startup  │"
+    echo "│  ${blue}x-ui log${plain}          - Check logs                       │"
+    echo "│  ${blue}x-ui banlog${plain}       - Check Fail2ban ban logs          │"
+    echo "│  ${blue}x-ui update${plain}       - Update                           │"
+    echo "│  ${blue}x-ui legacy${plain}       - legacy version                   │"
+    echo "│  ${blue}x-ui install${plain}      - Install                          │"
+    echo "│  ${blue}x-ui uninstall${plain}    - Uninstall                        │"
+    echo "└───────────────────────────────────────────────────────┘"
 }
 
-echo -e "${green}运行中...${plain}"
+echo "${green}Running...${plain}"
 install_base
-install_x-ui $1
+install_x-ui "$1"
